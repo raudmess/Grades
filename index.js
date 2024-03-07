94db71d768fae65917d713eed5af0127bf67d9c5
Mohamed Gamal <90609678+raudmess@users.noreply.github.com>
2024-03-07 07:48:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let testScore = 76;
+let letterGrade;
+
+switch(true) {
+    case testScore >= 90: 
+    letterGrade = "A";
+    break;
+    case testScore >=80:
+    letterGrade = "B";
+    break;
+    case testScore >=70:
+    letterGrade = "C";
+    break;
+    case testScore >=60:
+    letterGrade = "D";
+    default:
+    letterGrade= "F";
+}
+console.log(letterGrade);
+
+
